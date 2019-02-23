5f6f5c76c1c9690b1040d475a1398598829a19b7
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-02-23 13:37:29 +0530

    Added open-source projects

--- src/pages/work.js
+++ src/pages/work.js
@@ -7,7 +7,13 @@ import SEO from '../components/seo'
 const WorkPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1 style={{ fontSize: '2em' }}>The projects I've worked on so far</h1>
+    <h2 style={{ marginBottom: 10 }}>The projects I've worked on so far</h2>
+    <p>
+      I cannot show some of my best work here due to non disclosure agreements.
+      I have worked with enterprise clients and built multi-lingual sites for
+      goverment organistaions. Here are some of my latests works I'm allowed to
+      display.
+    </p>
     <section
       style={{
         display: 'flex',
@@ -57,6 +63,18 @@ const WorkPage = () => (
           <span>WordPress</span>
         </div>
       </div>
+    </section>
+
+    <h2 style={{ marginTop: 20, marginBottom: 10 }}>
+      I have written for certain sites as well
+    </h2>
+    <section
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+      }}
+    >
       <div className="work-project">
         <a href="https://www.smashingmagazine.com/author/muhammad-muhsin/">
           <img
@@ -84,6 +102,54 @@ const WorkPage = () => (
         <div className="project-technologies">
           <span>React</span> <span>Gatsby</span>
         </div>
+      </div>
+    </section>
+
+    <h2 style={{ marginTop: 20, marginBottom: 10 }}>
+      Some of my open source projects
+    </h2>
+    <section
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <div className="work-project open-source">
+        <a
+          href="https://github.com/m-muhsin/celestial"
+          style={{ color: 'white', textAlign: 'left' }}
+        >
+          <h3 className="open-source-title">celestial</h3>
+          <p className="open-source-desc">
+            A WordPress theme built with React.
+          </p>
+        </a>
+      </div>
+
+      <div className="work-project open-source">
+        <a
+          href="https://github.com/m-muhsin/woocommerce-next"
+          style={{ color: 'white', textAlign: 'left' }}
+        >
+          <h3 className="open-source-title">woocommerce-next</h3>
+          <p className="open-source-desc">
+            A Next.js (Server-side React) store that fetches fata from
+            WooCommcere.
+          </p>
+        </a>
+      </div>
+
+      <div className="work-project open-source">
+        <a
+          href="https://github.com/laccadive-io/react-native-woocommerce-store"
+          style={{ color: 'white', textAlign: 'left' }}
+        >
+          <h3 className="open-source-title">react -native-woocommerce-store</h3>
+          <p className="open-source-desc">
+            A React Native app that fetches data from WooCommerce.
+          </p>
+        </a>
       </div>
     </section>
   </Layout>
