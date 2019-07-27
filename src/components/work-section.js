7587bca6936ef6731363966ba960add066834819
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-07-27 10:36:00 +0530

    add margin top to conf name

--- src/components/work-section.js
+++ src/components/work-section.js
@@ -166,7 +166,7 @@ export default () => (
             alt="JS for WP Conf 2019"
           />
         </a>
-          <h4 style={{ marginBottom: 10 }}>JS for WP Conf 2019</h4>
+          <h4 style={{ marginTop:10, marginBottom: 10 }}>JS for WP Conf 2019</h4>
           <h5  style={{ marginBottom: 10 }}>Server Side React for WordPress</h5>
           <div className="project-technologies">
             <span>July 12, 2019</span> <span><a style={{ color: 'black', fontWeight: 'bold' }} href="https://www.slideshare.net/rtcamp/server-side-react-for-wordpress-by-muhammad-muhsin-the-javascript-for-wordpress-conference-jsforwpconf-12-jul-2019">Slides</a></span>
@@ -180,7 +180,7 @@ export default () => (
             alt="Gatsby"
           />
         </a>
-          <h4 style={{ marginBottom: 10 }}>GatsbyJS Montréal Meetup #1</h4>
+          <h4 style={{ marginTop:10, marginBottom: 10 }}>GatsbyJS Montréal Meetup #1</h4>
           <h5  style={{ marginBottom: 10 }}>Building with Gatsby and WordPress</h5>
           <div className="project-technologies">
             <span>July 12, 2019</span> <span><a style={{ color: 'black', fontWeight: 'bold' }} href="https://gatsbyjs-montreal-meetup.muhammad.dev">Slides</a></span>
@@ -194,7 +194,7 @@ export default () => (
             alt="Gatsby"
           />
         </a>
-          <h4 style={{ marginBottom: 10 }}>WooSesh 2018</h4>
+          <h4 style={{ marginTop:10, marginBottom: 10 }}>WooSesh 2018</h4>
           <h5  style={{ marginBottom: 10 }}>Creating a WooCommerce Mobile App with React Native</h5>
           <div className="project-technologies">
             <span>October 18, 2018</span> <span><a style={{ color: 'black', fontWeight: 'bold' }} href="https://docs.google.com/presentation/d/1TKkt9M8RViageCS7whYPafjVdmLtgNQ2Bd9taCLjrLU/present">Slides</a></span>
