26ddf47437f516db151fa04b31541e7b622e92ed
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2020-04-26 12:44:54 +0530

    check for window object not undefined

--- src/utils/strip-html.js
+++ src/utils/strip-html.js
@@ -1,6 +1,7 @@
 export default function stripHtml(html)
 {
-   var tmp = document.createElement("DIV");
+   if ( typeof window === 'undefined' ) return
+   let tmp = document.createElement("DIV");
    tmp.innerHTML = html;
    return tmp.textContent || tmp.innerText || "";
 }
