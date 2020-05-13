9f8eb71e98f38b1dfd7d716531a17701f893eaa9
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2020-05-13 16:21:49 +0530

    basinc search working

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,29 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const GET_COMMENTS = gql`
+  query SearchPostsQuery($searchTerm: String!) {
+    posts(where: { search: $searchTerm }) {
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`
 
 const Search = () => {
   const inputEl = useRef(null)
+  const [searchKey, setSearchKey] = useState('')
   const [showSearchBar, setShowSearchBar] = useState(false)
   const [showSearchResults, setShowSearchResults] = useState(false)
+
+  const { data: queriedData } = useQuery(GET_COMMENTS, {
+    variables: { searchTerm: searchKey },
+  })
+
+  console.log('queriedData', queriedData)
 
   useEffect(() => {
     if (showSearchBar) {
@@ -16,13 +36,19 @@ const Search = () => {
   }
 
   const toggleShowSearchResults = (event) => {
-      const searchKey = inputEl.current.value
-      if(searchKey.length > 0) {
-          setShowSearchResults(true)
-      }
-      if(searchKey.length === 0) {
-          setShowSearchResults(false)
-      }
+    setSearchKey(inputEl.current.value)
+    if (searchKey.length > 0) {
+      setShowSearchResults(true)
+    }
+    if (searchKey.length === 0) {
+      setShowSearchResults(false)
+    }
+  }
+
+  const onCloseSearchBar = () => {
+    inputEl.current.value = ''
+    setShowSearchResults(false)
+    setShowSearchBar(false)
   }
 
   return (
@@ -33,6 +59,7 @@ const Search = () => {
         </span>
       </button>
       <form
+        onSubmit={(e) => e.preventDefault()}
         style={{
           visibility: showSearchBar ? 'visible' : 'hidden',
           position: 'absolute',
@@ -49,11 +76,23 @@ const Search = () => {
           type="text"
           onChange={(event) => toggleShowSearchResults(event)}
         />
+        <button
+          style={{
+            cursor: 'pointer',
+            position: 'absolute',
+            right: 9,
+            top: 5,
+          }}
+          className="search-x-btn"
+          onClick={() => onCloseSearchBar()}
+        >
+          ✕
+        </button>
       </form>
       <div
         style={{
           backgroundColor: 'gray',
-          position: 'fixed',
+          position: 'absolute',
           marginTop: 50,
           left: 0,
           width: '100%',
@@ -63,6 +102,13 @@ const Search = () => {
         }}
       >
         Search results
+        <div>
+          {!!searchKey &&
+            queriedData?.posts?.nodes &&
+            queriedData.posts.nodes.map((post) => (
+              <h4 dangerouslySetInnerHTML={{ __html: post.title }} />
+            ))}
+        </div>
       </div>
     </>
   )
