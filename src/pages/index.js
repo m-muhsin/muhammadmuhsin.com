0a823cd4aa1fd470fadf3236470c2f77a2021824
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2020-05-14 08:52:41 +0530

    improve home SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -13,26 +14,45 @@ import {
   Contact,
 } from '../components/homepage-parts'
 
-const IndexPage = () => (
-  <Layout location="home">
-    <SEO
-      title="Muhammad.dev"
-      keywords={[
-        `muhammad muhsin`,
-        `full stack developer`,
-        `wordpress`,
-        `react`,
-      ]}
-    />
-    <Intro />
-    <Work />
-    <Writing />
-    <Speaking />
-    <OpenSource />
-    <Featured />
-    <Testimonials />
-    <Contact />
-  </Layout>
-)
+const IndexPage = ({ data }) =>
+  console.log('data', data) && (
+    <Layout location="home">
+      <SEO
+        title="muhammad.dev"
+        description={
+          data.site.siteMetadata.description ||
+          `Muhammad Muhsin's space on the web`
+        }
+        image={data.site.siteMetadata.image}
+        keywords={[
+          `muhammad muhsin`,
+          `full stack developer`,
+          `wordpress`,
+          `react`,
+        ]}
+      />
+      <Intro />
+      <Work />
+      <Writing />
+      <Speaking />
+      <OpenSource />
+      <Featured />
+      <Testimonials />
+      <Contact />
+    </Layout>
+  )
+
+export const query = graphql`
+  query HomeSEOQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        image
+      }
+    }
+  }
+`
 
 export default IndexPage
